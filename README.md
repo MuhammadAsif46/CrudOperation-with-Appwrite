4ae65a6d07a2adc236ca905d590b5352aa260910
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-05-18 11:59:51 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CrudOperation-with-Appwrite
+ I am create complete crud-operation from user reading all post or add post, edit and delete post with appwrite backend as a service using vite and tailwind css.
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <h1 className='text-3xl p-4 bg-purple-500 text-white'>Crud Operation with Appwrite</h1>
+   </>
+  )
+}
+
+export default App
