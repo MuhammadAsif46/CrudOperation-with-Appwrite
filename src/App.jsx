e866dab8dd5141ae6f788e0241b190d406d3960b
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-05-19 13:38:55 +0500

    added post form work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import PostForm from './components/PostForm/PostForm'
 
 function App() {
 
   return (
    <>
-   <h1 className='text-3xl p-4 bg-purple-500 text-white'>Crud Operation with Appwrite</h1>
+   <h1 className='text-3xl p-2 bg-purple-500 text-white'>Crud Operation with Appwrite</h1>
+    <PostForm/>
    </>
   )
 }
---
+++ src/components/PostForm/PostForm.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import profileImg from "../../assets/user.svg";
+
+const PostForm = () => {
+  return (
+    <div className="flex justify-center items-center py-3">
+      <div className="bg-white rounded-xl shadow-lg w-1/2 p-2">
+        <div className="flex p-2 items-center gap-3">
+          <div
+            className="rounded-full flex justify-center items-center"
+            style={{ border: "1px solid black", width: "50px", height: "50px" }}
+          >
+            <img src={profileImg} className="" alt="profile-img" />
+          </div>
+          <div className="flex flex-col gap-1">
+            <span className="text-lg">Muhammad Asif</span>
+            <span className="text-sm text-gray-500">
+              19-06-2024<strong> 12 : 45</strong>
+            </span>
+          </div>
+        </div>
+        <div className="p-2">
+          <textarea rows={5} className="border w-full"></textarea>
+        </div>
+        <div className="p-2">
+        <button type="button" className="focus:outline-none w-full text-white bg-purple-500 hover:bg-purple-700 focus:ring-1 focus:ring-purple-300 font-medium rounded-lg px-5 py-2.5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Post</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostForm;
